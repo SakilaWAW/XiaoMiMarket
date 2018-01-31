f95834431d503857dd02dc39d7cd05c99ecee3e4
SakilaWAW <pccp0909@163.com>
2018-01-31 22:50:05 +0800

    完成ExpandMenu的静态布局

--- src/components/HeaderAreaComp/ExpandMenu.vue
+++ src/components/HeaderAreaComp/ExpandMenu.vue
@@ -1,29 +1,40 @@
 <template>
   <div class="expand-menu">
     <ul class="menu-container">
-      <li class="menu-item" v-for="item in items">
-        <div>
-
-        </div>
+      <li class="menu-item"
+          v-for="(item, index) in items"
+          :key="item.name"
+          :class="{'right-sep-line': index<5}">
+        <span class="top-des" v-if="item.hasDes">{{ item.topDes }}</span>
+        <a class="item-img" :href="`https://www.mi.com/${item.hrefPostfix}`" :style="{background: `url(${item.img})`}"></a>
+        <a class="item-name" :href="`https://www.mi.com/${item.hrefPostfix}`">{{ item.name }}</a>
+        <span :class="{'orange-text': isOrangeDes(item.name)}">{{ item.bottomDes }}</span>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+const phonePic = require('../../assets/phone.png');
+
 export default {
   name: 'expand-menu',
   data() {
     return {
       items: [
-        { isNew: true, img: '', name: '红米Note3', value: '1233元起' },
-        { isNew: true, img: '', name: '小米7', value: '2013元起' },
-        { isNew: false, img: '', name: '红米4', value: '1399元起' },
-        { isNew: false, img: '', name: '小米Mix2', value: '3999元起' },
-        { isNew: false, img: '', name: '小米Mix2', value: '3999元起' },
-        { isNew: false, img: '', name: '小米Mix2', value: '3999元起' },
+        { hasDes: true, topDes: '新品', img: phonePic, name: '红米Note3', bottomDes: '1233元起', hrefPostfix: 'mi5x' },
+        { hasDes: true, topDes: '新品', img: phonePic, name: '小米7', bottomDes: '2013元起', hrefPostfix: 'mi5x' },
+        { hasDes: false, topDes: '', img: phonePic, name: '红米4', bottomDes: '1399元起', hrefPostfix: 'mi5x' },
+        { hasDes: false, topDes: '', img: phonePic, name: '小米Mix2', bottomDes: '3999元起', hrefPostfix: 'mi5x' },
+        { hasDes: false, topDes: '', img: phonePic, name: '小米Mix2', bottomDes: '3999元起', hrefPostfix: 'mi5x' },
+        { hasDes: false, topDes: '', img: phonePic, name: '查看全部', bottomDes: '小米手机', hrefPostfix: 'mi5x' },
       ],
     };
+  },
+  methods: {
+    isOrangeDes(name) {
+      return name !== '查看全部';
+    },
   },
 };
 </script>
@@ -34,23 +45,60 @@ export default {
   .expand-menu {
     width: 100%;
     height: $menu-height;
-    background: yellowgreen;
+    background: white;
+    border-top: 1px solid $shadow-grey;
+    box-shadow: 0 5px 4px -4px $shadow-grey;//单侧投影技巧
   }
   .menu-container {
     display: flex;
     width: $site-content-width;
     height: $menu-height;
     margin: 0 auto;
-    background: indianred;
   }
 
   .menu-item {
+    position: relative;
+    text-align: center;
     width: 201px;
     margin-right: 5px;
     height: $menu-height;
-    background:yellow;
+    font-size: 12px;
   }
   .menu-item:nth-child(1) {
     margin-left: 5px;
+  }
+  .top-des {
+    border: 1px solid $mi-orange;
+    line-height: 22px;
+    height: 22px;
+    width: 66px;
+    position: absolute;
+    top: -1px;
+    right: 50%;
+    transform: translateX(50%);
+    color: $mi-orange;
+  }
+  .item-img {
+    height: 110px;
+    width: 160px;
+    display: block;
+    margin: 40px auto 16px auto;
+  }
+  .item-name {
+    margin-bottom: 2px;
+    color: black;
+    display: block;
+  }
+  .orange-text {
+    color: $mi-orange;
+  }
+  .right-sep-line::after {
+    content: "";
+    width: 1px;
+    height: 100px;
+    position: absolute;
+    top: 45px;
+    right: 0;
+    background: $shadow-grey;
   }
 </style>
