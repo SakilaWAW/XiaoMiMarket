c2a052bae1e14d773413a7c97f03aafe8146dd59
sakilawaw <pccp0909@163.com>
2018-02-07 19:58:53 +0800

    完成StarProductList基本布局。

--- src/components/ContentAreaComp/StarProductList/ScrollPage.vue
+++ src/components/ContentAreaComp/StarProductList/ScrollPage.vue
@@ -2,7 +2,10 @@
   <div class="scroll-page">
     <ul class="container" :style="{width: ulWidth + 'px'}">
       <li class="star-item" v-for="(item, index) in itemList" :key="index">
-
+        <a class="item-href-pic" :href="item.href" :style="{background: `url(${item.img})`}"></a>
+        <span class="item-name"><a :href="item.href">{{ item.name }}</a></span>
+        <span class="item-desc">{{ item.desc }}</span>
+        <span class="item-sub-desc">{{ item.subDesc }}</span>
       </li>
     </ul>
   </div>
@@ -14,27 +17,57 @@ export default {
   props: ['itemList'],
   computed: {
     ulWidth() {
-      return (234 * this.itemList.length) + (17.5 * (this.itemList.length - 1));
+      return (this.itemList.length / 5) * 1240;
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+  @import '../../../assets/css/global';
+  $shallow-grey: #FAFAFA;
+  $text-grey: #AAA;
   .scroll-page {
     width: 100%;
     height: 340px;
-    border: 1px solid red;
     overflow: hidden;
     display: flex;
     justify-content: space-between;
     font-size: 0;
+    margin-bottom: 30px;
+  }
+  .container {
+    text-align: center;
   }
   .star-item {
     font-size: 16px;
     display: inline-block;
     width: 234px;
+    margin-right: 17.5px;
     height: 100%;
-    background: red;
+    background: $shallow-grey;
+  }
+  .star-item:nth-child(5n) {
+    margin-right: 0;
+  }
+  .item-href-pic, .item-name, .item-desc, .item-sub-desc {
+    display: block;
+  }
+  .item-href-pic {
+    margin: 40px auto 30px auto;
+    height: 160px;
+    width: 160px;
+  }
+  .item-name a {
+    color: black;
+  }
+  .item-desc {
+    margin-top: 3px;
+    margin-bottom: 12px;
+    font-size: 12px;
+    color: $text-grey;
+  }
+  .item-sub-desc {
+    color: $mi-orange;
   }
 </style>
